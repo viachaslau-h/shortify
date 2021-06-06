c032d668076ce024d81e37b11e363bbccdc50713
Gwan <slavik.art15@gmail.com>
2021-06-06 11:22:12 +0300

    Init application

---
+++ client/src/pages/AuthPage.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const AuthPage = () => (
+    <div className="row">
+        <div className="col s6 offset-s3">
+            <h1>Сократи Ссылку</h1>
+            <div className="card blue darken-1">
+              <div className="card-content white-text">
+                <span className="card-title">Авторизация</span>
+                <div>
+
+                </div>
+              </div>
+              <div className="card-action">
+                  <button className="btn yellow darken-4" style={{marginRight: 10}}>Войти</button>
+                  <button className="btn gray lighten-1 black-text">Регистрация</button>
+              </div>
+          </div>
+        </div>
+    </div>
+)
