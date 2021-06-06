dfdbee669b402c0a5b1d4bc263feb31b898496b5
Gwan <slavik.art15@gmail.com>
2021-06-06 21:49:44 +0300

    First commit

--- client/src/routes.js
+++ client/src/routes.js
@@ -6,29 +6,29 @@ import {DetailPage} from './pages/DetailPage'
 import {AuthPage} from './pages/AuthPage'
 
 export const useRoutes = isAuthenticated => {
-    if (isAuthenticated) {
-        return (
-            <Switch>
-             <Route path="/links" exact>
-                 <LinksPage />
-             </Route>
-             <Route path="/create" exact>
-                 <CreatePage />
-             </Route>
-             <Route path="/detail/:id">
-                 <DetailPage />
-             </Route>
-             <Redirect to="/create" />
-            </Switch>
-        )
-    }
-
+  if (isAuthenticated) {
     return (
-        <Switch>
-            <Route path="/" exact>
-                <AuthPage />
-            </Route>
-            <Redirect to="/" />
-        </Switch>
+      <Switch>
+        <Route path="/links" exact>
+          <LinksPage />
+        </Route>
+        <Route path="/create" exact>
+          <CreatePage />
+        </Route>
+        <Route path="/detail/:id">
+          <DetailPage />
+        </Route>
+        <Redirect to="/create" />
+      </Switch>
     )
+  }
+
+  return (
+    <Switch>
+      <Route path="/" exact>
+        <AuthPage />
+      </Route>
+      <Redirect to="/" />
+    </Switch>
+  )
 }
